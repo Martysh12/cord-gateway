59d4408941603a856f777ae9584fa17222365b70
Martin K <martyshkon@gmail.com>
2022-08-15 22:30:11 +0300

    Empty blue and orange commands

--- bot.py
+++ bot.py
@@ -9,10 +9,25 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix="gate?")
 
+gateway = {
+    "blue"  : None,
+    "orange": None
+}
+
 @bot.event
 async def on_ready():
     print(f'Ready! Logged in as {bot.user} (ID: {bot.user.id})')
     await bot.close()
+
+@bot.command()
+async def blue(ctx):
+    """Open a blue portal"""
+    pass
+
+@bot.command()
+async def orange(ctx):
+    """Open an orange portal"""
+    pass
 
 bot.run(os.getenv("TOKEN"))
 
