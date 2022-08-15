60bbbb4e6a55176370ecf89150c0925791e0d70c
Martin K <martyshkon@gmail.com>
2022-08-15 23:16:03 +0300

    Relay messages

--- bot.py
+++ bot.py
@@ -14,14 +14,42 @@ gateway = {
     "orange": None
 }
 
+ignore_next_message = False
+
 @bot.event
 async def on_ready():
     print(f'Ready! Logged in as {bot.user} (ID: {bot.user.id})')
+
+@bot.listen("on_message")
+async def message_relayer(message):
+    if (gateway["blue"] is None) or (gateway["orange"] is None):
+        return
+
+    if message.author == bot.user:
+        return
+
+    if message.channel.id in gateway.values():
+        global ignore_next_message
+
+        if ignore_next_message:
+            ignore_next_message = False
+            return
+
+        if message.channel.id == gateway["blue"]:
+            other_channel = await bot.fetch_channel(gateway["orange"])
+            await other_channel.send(message.content)
+
+        if message.channel.id == gateway["orange"]:
+            other_channel = await bot.fetch_channel(gateway["blue"])
+            await other_channel.send(message.content)
 
 @bot.command()
 async def blue(ctx):
     """Open a blue portal"""
     gateway["blue"] = ctx.channel.id
+
+    global ignore_next_message
+    ignore_next_message = True
 
     print(f"Portal bound: BLUE   -- {repr('#' + ctx.channel.name)} :: {repr(ctx.channel.guild.name)}")
     await ctx.send("A blue portal has been spawned.")
@@ -30,6 +58,9 @@ async def blue(ctx):
 async def orange(ctx):
     """Open an orange portal"""
     gateway["orange"] = ctx.channel.id
+
+    global ignore_next_message
+    ignore_next_message = True
 
     print(f"Portal bound: ORANGE -- {repr('#' + ctx.channel.name)} :: {repr(ctx.channel.guild.name)}")
     await ctx.send("An orange portal has been spawned.")
